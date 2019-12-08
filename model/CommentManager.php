819a77687545f3e275c10b805f8baaeb324e14b3
Flawxy <58270365+Flawxy@users.noreply.github.com>
2019-12-08 12:07:01 +0100

    CommentManager & PostManager modifications
    Added new functions to CommentManager & PostManager

--- model/CommentManager.php
+++ model/CommentManager.php
@@ -3,5 +3,19 @@ require_once('model/Manager.php');
 
 class CommentManager extends Manager
 {
+    public function getComments($postId)
+    {
+        $db = $this->databaseConnect();
+        $comments = $db->prepare('SELECT comment_id, comment_author, comment_content, DATE_FORMAT(comment_date, \'%d/%m/%y - %Hh%imin%ss\') AS comment_date_fr FROM comments_table WHERE related_post_id = ?');
+        $comments->execute(array($postId));
 
+        return $comments;
+    }
+
+    public function postComment($postId, $author, $content)
+    {
+        $db = $this->databaseConnect();
+        $comments = $db->prepare('INSERT INTO comments_table(related_post_id, comment_author, comment_content, comment_date) VALUES(?, ?, ?, NOW())');
+        $comments->execute(array($postId, $author, $content));
+    }
 }
--- model/PostManager.php
+++ model/PostManager.php
@@ -3,5 +3,19 @@ require_once('model/Manager.php');
 
 class PostManager extends Manager
 {
+    public function getPosts()
+    {
+        $db = $this->databaseConnect();
 
+        return $db->query('SELECT post_id, post_title, post_content, DATE_FORMAT(post_date, \'%d/%m/%y - %Hh%imin%ss\') AS posts_dates_fr FROM posts_table ORDER BY post_date');
+    }
+
+    public function getPost($postId)
+    {
+        $db = $this->databaseConnect();
+        $req = $db->prepare('SELECT post_id, post_title, post_content, DATE_FORMAT(post_date, \'%d/%m/%y - %Hh%imin%ss\') AS posts_dates_fr FROM posts_table WHERE post_id = ?');
+        $req->execute(array($postId));
+
+        return $req->fetch();
+    }
 }
