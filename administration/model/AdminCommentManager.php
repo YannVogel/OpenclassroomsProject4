2d40db72ef1831a2132375a87ab7402ce403a279
Flawxy <58270365+Flawxy@users.noreply.github.com>
2019-12-08 12:26:34 +0100

    Created admin section
    Added admin section base files

---
+++ administration/model/AdminCommentManager.php
@@ -0,0 +1,16 @@
+<?php
+require_once('model/CommentManager.php');
+
+class AdminCommentManager extends CommentManager
+{
+    private function displayReportedComments()
+    {
+        //display a list of reported comments
+    }
+
+    private function deleteComment($commentId)
+    {
+        //delete a selected comment
+    }
+
+}
