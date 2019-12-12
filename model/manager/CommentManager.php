5fe07fc532b9946b8d99873b4e61b3859bfe9d1a
Flawxy <58270365+Flawxy@users.noreply.github.com>
2019-12-12 08:19:40 +0100

    Started merging managers
    Started merging admin managers & "normal" managers

--- model/manager/CommentManager.php
+++ model/manager/CommentManager.php
@@ -7,13 +7,13 @@ use Project\Model\Manager\Manager;
 
 class CommentManager extends Manager
 {
-    public function getComments($postId)
+    public function getComments(int $postId)
     {
         $db = $this->databaseConnect();
         $comments = $db->prepare('SELECT comment_id, comment_author, comment_content, DATE_FORMAT(comment_date, \'%d/%m/%y - %Hh%imin%ss\') AS comment_date_fr FROM comments_table WHERE related_post_id = ?');
         $comments->execute(array($postId));
 
-        return $comments;
+        return $comments->fetchAll(\PDO::FETCH_CLASS, CommentEntity::class);
     }
 
     public function addComment(CommentEntity $comment)
@@ -21,5 +21,19 @@ class CommentManager extends Manager
         $db = $this->databaseConnect();
         $comments = $db->prepare('INSERT INTO comments_table(related_post_id, comment_author, comment_content, comment_date) VALUES(?, ?, ?, NOW())');
         $comments->execute(array($comment->getCommentId(), $comment->getCommentAuthor(), $comment->getCommmentContent()));
+    }
+
+    public function displayReportedComments()
+    {
+        $db = $this->databaseConnect();
+        $comments = $db->prepare('SELECT comment_id, related_post_id, comment_author, comment_content, DATE_FORMAT(comment_date, \'%d/%m/%y - %Hh%imin%ss\') AS comment_date_fr FROM comments_table ORDER BY comment_moderation DESC');
+        $comments->execute();
+
+        return $comments;
+    }
+
+    public function deleteComment(int $commentId)
+    {
+        //delete a selected comment
     }
 }
