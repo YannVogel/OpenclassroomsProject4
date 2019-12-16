f210e10c12c44d71f6ebd187c5a566e82bf63d96
Flawxy <58270365+Flawxy@users.noreply.github.com>
2019-12-16 12:10:25 +0100

    created headers separated files
    Created header.php & adminHeader.php

---
+++ adminHeader.php
@@ -0,0 +1,7 @@
+<header class="container">
+    <div class="col text-center">HEADER DU SITE
+        <div>Vous êtes connecté·e en tant que <?= $_SESSION['nickname'] ?></div>
+        <button id="endConnectionButton" class="btn btn-primary">Se déconnecter<span class="fa faButtonRight fa-toggle-off"></span></button>
+        <button id="adminPanelQuitButton" class="btn btn-success">Page d'accueil<span class="fa faButtonRight fa-home"></span></button>
+    </div>
+</header>
