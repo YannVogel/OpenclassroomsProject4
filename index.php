d842d5f47338c18cd5d4faa51fd6b223d27e255b
Flawxy <58270365+Flawxy@users.noreply.github.com>
2019-12-19 08:03:10 +0100

    created templateView

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
+if(!isset($_SESSION)) {
+    session_start();
+}
 require "vendor/autoload.php";
 
 use Project\Controller\PostController;
@@ -7,26 +9,9 @@ use Project\Controller\CommentController;
 use Project\Controller\AdminPostController;
 use Project\Controller\AdminCommentController;
 use Project\Controller\AdminUserController;
-?>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="public/css/style.css" />
-    <script src="https://cdn.tiny.cloud/1/eao0m8gx6g8dp0oieeo8wvkd3ut5slygyly5rgcyizps38du/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
-    <script>tinymce.init({selector:'textarea'});</script>
-    <title>Le blog de Jean Forteroche</title>
-</head>
-<body>
-<?php
-include('view/alertsView.php');
+
 
 $myAdmin = new AdminUserController();
 if(isset($_POST['nicknameInput']) AND isset($_POST['passwordInput']) AND $myAdmin->isConnectionValid($_POST['nicknameInput'], $_POST['passwordInput']))
@@ -77,6 +62,8 @@ if(isset($_GET['deletePostId']) AND trim($_GET['deletePostId']) !== "")
 
 }
 
+$controller = new PostController();
+
 
 if(isset($_GET['displayPost']) AND trim($_GET['displayPost']) !== "") {
 
@@ -87,21 +74,9 @@ if(isset($_GET['displayPost']) AND trim($_GET['displayPost']) !== "") {
     require('view/adminView.php');
 
 }else {
-    require('view/indexView.php');
+
+    $pageContent = $controller->getHome();
 }
 
-include('view/connectionView.php');
-include('view/inscriptionView.php');
-?>
+include('view/templateView.php');
 
-
-
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-<script src="public/js/Storage.js"></script>
-<script src="public/js/ActiveTab.js"></script>
-<script src="public/js/javascript.js"></script>
-</body>
-</html>
--- view/indexView.php
+++ view/indexView.php
@@ -1,10 +1,4 @@
-<?php
-use Project\Controller\PostController;
-use Project\Controller\AdminUserController;
-
-$myAdmin = new AdminUserController();
-
-include('headerView.php');
-
-    $manage = new PostController;
-    $manage->displayLastPost();
+<section class="container">
+    <div class="col col-12 text-center"><h2 class="h2"><?= $lastPost->getPostTitle(); ?></h2> le <?= $lastPost->getPostDate(); ?></div>
+    <div class="col"><?= $lastPost->getPostIntro(); ?></div>
+</section>
