f7e9b3024ce5c26544bedc2642956a611e9ca6b2
Flawxy <58270365+Flawxy@users.noreply.github.com>
2019-12-21 08:15:33 +0100

    Merged indexView & faqView
    faqView no longer exists and it's past content is now part of the home page.

--- inscription.php
+++ inscription.php
@@ -1,14 +1,10 @@
 <?php
-/* nicknameInscriptionInput
- * passwordInscriptionInput
- * passwordInscriptionConfirmationInput
- */
 require "vendor/autoload.php";
 
 use Project\Controller\AdminUserController;
 
 $regex = "#^[a-z]+[a-z1-9éèçëïüÿà_-]*[a-z1-9éèçëïüÿà]$#i";
-$req = new AdminUserController();
+$controller = new AdminUserController();
 
 $confirmation1 = isset($_POST['nicknameInscriptionInput']);
 $confirmation2 = isset($_POST['passwordInscriptionInput']);
@@ -20,26 +16,26 @@ $confirmation6 = trim($_POST['passwordInscriptionConfirmationInput']) !== "";
 
 if( $confirmation1 AND $confirmation2 AND $confirmation3 AND $confirmation4 AND $confirmation5 AND $confirmation6)
 {
-    if (!$req->isNicknameAvailable($_POST['nicknameInscriptionInput'])) {
+    if (!$controller->isNicknameAvailable($_POST['nicknameInscriptionInput'])) {
 
         echo 'Ce pseudonyme n\'est pas disponible !';
-        $req->failureAddUser();
+        $controller->failureAddUser();
 
     } elseif (!preg_match($regex, $_POST['nicknameInscriptionInput'])) {
 
         echo 'Le format du pseudo ' . $_POST['nicknameInscriptionInput'] . ' n\'est pas accepté';
-        $req->failureAddUser();
+        $controller->failureAddUser();
 
-    } elseif (!$req->doPasswordsMatch($_POST['passwordInscriptionInput'], $_POST['passwordInscriptionConfirmationInput'])) {
+    } elseif (!$controller->doPasswordsMatch($_POST['passwordInscriptionInput'], $_POST['passwordInscriptionConfirmationInput'])) {
 
         echo 'Les mdp entrés ne correspondent pas !';
-        $req->failureAddUser();
+        $controller->failureAddUser();
 
     }else {
-        $req->valideAddUser();
+        $controller->valideAddUser();
     }
 
 }else {
     echo 'Merci de remplir toutes les données du formulaire afin de valider votre inscription';
-    $req->failureAddUser();
+    $controller->failureAddUser();
 }
