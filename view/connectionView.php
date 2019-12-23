984a215b96d11ac3f56a8d642f0e2b9240634a32
Flawxy <58270365+Flawxy@users.noreply.github.com>
2019-12-23 12:22:31 +0100

    Headers corrections for SEO
    title.

--- view/connectionView.php
+++ view/connectionView.php
@@ -3,7 +3,7 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h4 class="modal-title">Vos identifiants :</h4>
+                    <div class="modal-title">Vos identifiants :</div>
                     <button type="button" class="close" data-dismiss="modal">
                         <span>&times;</span>
                     </button>
